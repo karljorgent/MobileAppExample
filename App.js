bf21214f6d4c06e134f666663738cce06ed68337
karljorgent <karl-jorgen.tormet@khk.ee>
2024-03-25 09:22:30 +0200

    Create authorization screens

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import {SafeAreaView} from 'react-native'
-import Splash from './src/screens/auth/Splash'
+import React, { useEffect } from 'react';
+import {SafeAreaView} from 'react-native';
+import Signup from './src/screens/auth/Signup';
+//import AuthHeader from './src/components/AuthHeader';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 const App = () => {
-  return (
-    <SafeAreaView>
-      <Splash />
-    </SafeAreaView>
-  )
-}
+    useEffect(() => {
+        GoogleSignin.configure({
+            scopes: ['https://www.googleapis.com/auth/drive.readonly'],
+            webClientId: '606623206800-tlmuvn2mq4u103pb0blq28dpn1immr78.apps.googleusercontent.com',
+            offlineAccess: true
+        });
+    }, [])
+    return (
+        <SafeAreaView>
+        <Signup />
+        </SafeAreaView>
+    );
+};
 
-export default App
+export default App;
